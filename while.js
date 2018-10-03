733063b9f586998914cff075d78af2b5e38df302
Michael Stewart <mstewart@mstewart-l.corp.instructure.com>
2018-10-02 19:18:40 -0600

    Added while.html and while.js

---
+++ while.js
@@ -0,0 +1,38 @@
+//Set one
+var setOne = -10;
+
+while (setOne < 20){
+	console.log(setOne);
+	setOne++;
+}
+console.log("---")
+
+//Set two
+var setTwo = 10;
+
+while (setTwo <=  40){
+	console.log(setTwo);
+	setTwo+=2;
+}
+console.log("---")
+
+//Set three
+var setThree = 300;
+
+while (setThree <= 333){
+	if(setThree % 2 !== 0){
+		console.log(setThree);
+	}
+	setThree +=1;
+}
+console.log("---")
+
+//Set four
+var setFour = 5
+
+while(setFour <= 50){
+	if(setFour % 5 === 0 & setFour % 3 === 0){
+		console.log(setFour);
+	}
+	setFour+=1;
+}
